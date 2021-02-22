95ef1053f5099463fb1d89778d9e46993722910c
marcrowland <marcrowland@outlook.com>
2021-02-22 10:50:43 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace dotNetDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        }
+    }
+}
